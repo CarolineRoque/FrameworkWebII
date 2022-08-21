9bbb970bd56e5f09b24d5cf44c458f474b9c85ab
Caroline Ramos Roque <54049108+CarolineRoque@users.noreply.github.com>
2022-08-21 14:02:28 -0300

    Criando rotas

--- api_crud_viagens_mem/app.js
+++ api_crud_viagens_mem/app.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.get('/viagens', (req, res) => {
     res.send('Listando Viagens!')
   })
+
+app.post('/viagens', (req, res) =>{
+    res.send('Cadastrando Viagem!')
+})
+
+app.get('/viagens/:id', (req, res) => {
+    res.send('Buscando Viagem! '+req.params.id)
+  })
+
+app.put('/viagens/:id',(req, res) =>{
+    res.send('Atualizando Viagem! '+req.params.id)
+})  
+
+app.delete('/viagens/:id',(req, res) =>{
+    res.send('Removendo a Viagem! '+req.params.id)
+}) 
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
