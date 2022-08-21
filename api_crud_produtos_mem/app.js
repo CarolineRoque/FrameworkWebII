eb5f6339a49b4399e887366390426eac12324832
Caroline Ramos Roque <54049108+CarolineRoque@users.noreply.github.com>
2022-08-21 13:58:12 -0300

    Exercicio2

--- api_crud_produtos_mem/app.js
+++ api_crud_produtos_mem/app.js
@@ -2,13 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.get('/produtos', (req, res) => {
     res.send('Listando Produtos!')
   })
+
+app.post('/produtos', (req, res) =>{
+    res.send('Cadastrando Produtos!')
+})
+
+app.get('/produtos/:id', (req, res) => {
+    res.send('Buscando Produto! '+req.params.id)
+  })
+
+app.put('/produtos/:id',(req, res) =>{
+    res.send('Atualizando Produto! '+req.params.id)
+})  
+
+app.delete('/produtos/:id',(req, res) =>{
+    res.send('Removendo o Produto! '+req.params.id)
+}) 
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
